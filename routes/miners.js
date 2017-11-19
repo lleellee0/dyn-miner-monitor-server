17d9aaa40452df8bede39b6491eff60d58350514
데스크탑 <lleellee013@gmail.com>
2017-11-19 19:11:18 +0900

    해시레이트, 밸런스 합계 구현
    - api, 인덱스 페이지 표시 구현

--- routes/miners.js
+++ routes/miners.js
@@ -2,11 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 const miner_arr = new Array();
+const miner_arr_info = {};
 
 /* GET miners listing. */
 router.get('/', function(req, res, next) {
   res.json(miner_arr);
 });
+
+router.get('/info', function(req, res, next) {
+  miner_arr_info.hashrate = 0;
+  for(let i = 0; i < miner_arr.length; i++) {
+    miner_arr_info.hashrate += parseInt(miner_arr[i].hashrate);
+  }
+
+  miner_arr_info.balance = 0;
+  for(let i = 0; i < miner_arr.length; i++) {
+    miner_arr_info.balance += parseFloat(miner_arr[i].balance);
+  }
+
+  res.json(miner_arr_info);
+});
+
 
 router.post('/:miner_name/:hashrate/:balance', function(req, res, next) {
   for(let i = 0; i < miner_arr.length; i++) {
